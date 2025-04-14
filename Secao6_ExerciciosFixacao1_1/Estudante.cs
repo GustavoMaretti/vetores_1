08620ec13435dfd4019a58e97e0a35e1d53ea772
Gustavo Maretti <gustavohmaretti@outlook.com>
2025-04-14 11:33:30 -0300

    Projeto de gerenciamento de quartos de hotel com vetores

---
+++ Secao6_ExerciciosFixacao1_1/Estudante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Secao6_ExerciciosFixacao1_1
+{
+    internal class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
